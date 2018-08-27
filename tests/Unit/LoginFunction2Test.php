e0c3c017566661ee8aae6ee58a06952a3fc5de78
Trần Minh Đức <ductranminhitqb@gmail.com>
2018-08-27 11:28:09 +0700

    .

---
+++ tests/Unit/LoginFunction2Test.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\ManageUser;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class LoginFunction2Test extends TestCase
+{
+
+    use DatabaseTransactions;
+    use WithoutMiddleware;
+
+    private $users;
+    public function setUp()
+    {
+        parent::setUp();
+        $this->moking();
+    }
+    public function tearDown()
+    {
+        if($this->users){
+            $this->users->forceDelete();
+        }
+    }
+
+    /*
+     * create data default for test.
+     */
+    private function moking(){
+        $this->users = ManageUser::create(
+            [
+                'manage_role_id'      => \App\Models\ManageRole::ROLE_ADMIN,
+                'email'               => 'test@dac.co.jp',
+                'password'            => 'Password1234!@',
+                'user_last_name'      => 'Test',
+                'user_first_name'     => 'test',
+            ]
+        );
+    }
+
+    public function testLoginFailWithoutPass()
+    {
+        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
+        $response = $this->call('POST','/login', [
+            'email' => $admin->email,
+            'password' => ''
+        ]);
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->dontSeeIsAuthenticated();
+    }
+
+    public function testLoginFailWithInvalidUser()
+    {
+        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
+        $response = $this->call('POST','/login', [
+            'email' => $admin->email,
+            'password' => 'invalid'
+        ]);
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->dontSeeIsAuthenticated();
+    }
+
+    public function testLoginSuccess()
+    {
+        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
+        $this->actingAs($admin);
+        $response = $this->call('GET','/user');
+        $this->seeIsAuthenticated();
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
--- tests/Unit/LoginFunctionTest.php
+++ tests/Unit/LoginFunctionTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit;
 
-use App\Services\DemoService;
 use App\Models\ManageUser;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -11,76 +10,7 @@ use Tests\TestCase;
 class LoginFunctionTest extends TestCase
 {
     use DatabaseTransactions;
-    use WithoutMiddleware; // use this trait
-
-    private $users;
-
-    /*
-     * create data default for test.
-     */
-    private function getMock(){
-        $this->users = ManageUser::create(
-            [
-                'manage_role_id'      => \App\Models\ManageRole::ROLE_ADMIN,
-                'email'               => 'test@dac.co.jp',
-                'password'            => 'Password1234!@',
-                'user_last_name'      => 'Test',
-                'user_first_name'     => 'test',
-            ]
-        );
-    }
-
-    /*
-     * delete data test
-     */
-    private function deleteMock(){
-        if($this->users){
-            $this->users->forceDelete();
-        }
-    }
-
-    public function loginForTest()
-    {
-        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
-        $this->actingAs($admin);
-    }
-
-    public function testLoginSuccess()
-    {
-        $this->getMock();
-        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
-        $this->actingAs($admin);
-        $response = $this->call('GET','/user');
-        $this->seeIsAuthenticated();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->deleteMock();
-    }
-
-    public function testLoginFailWithoutPass()
-    {
-        $this->getMock();
-        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
-        $response = $this->call('POST','/login', [
-            'email' => $admin->email,
-            'password' => ''
-        ]);
-        $this->assertEquals(302, $response->getStatusCode());
-        $this->dontSeeIsAuthenticated();
-        $this->deleteMock();
-    }
-
-    public function testLoginFailWithInvalidUser()
-    {
-        $this->getMock();
-        $admin = ManageUser::where('email', 'test@dac.co.jp')->first();
-        $response = $this->call('POST','/login', [
-            'email' => $admin->email,
-            'password' => 'invalid'
-        ]);
-        $this->assertEquals(302, $response->getStatusCode());
-        $this->dontSeeIsAuthenticated();
-        $this->deleteMock();
-    }
+    use WithoutMiddleware;
     /**
      * An user without pass cannot be logged in.
      *
@@ -112,25 +42,12 @@ class LoginFunctionTest extends TestCase
         $this->dontSeeIsAuthenticated();
     }
 
-
-    /**
-     *
-     * @dataProvider dataForFunctionService
-     *
-     */
-    public function testFunctionService($data, $result)
+    public function testLoginSuccess()
     {
-        $this->getMock();
-        $this->loginForTest();
-        $this->assertEquals(DemoService::getData($data), $result);
-        $this->deleteMock();
-    }
-
-    public function dataForFunctionService(){
-        return [
-            [12, 12],
-            [true, true],
-            ['test_string', 'test_string']
-        ];
+        $user = factory(ManageUser::class)->create();
+        $this->actingAs($user);
+        $response = $this->call('GET','/user');
+        $this->seeIsAuthenticated();
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
